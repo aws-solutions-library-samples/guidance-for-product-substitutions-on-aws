ee29a4bfa42494c021aaeb4e94fae08d10c4a1f3
Ahmed Elsenousi <aelsenou@amazon.com>
2023-09-19 14:46:38 +0100

    add cloudscape

---
+++ ui/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@cloudscape-design/components', '@cloudscape-design/component-toolkit'],
+};
+
+module.exports = nextConfig;
